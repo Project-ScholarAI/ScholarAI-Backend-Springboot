dd4fc13f2163c29f54f1cade747dd2094180d889
Tasriad Ahmed Tias <trisn.eclipse@gmail.com>
2025-05-25 19:45:23 +0600

    Cross origin annotation added. (might not be needed)

--- src/main/java/dev/project/scholar_ai/ScholarAiApplication.java
+++ src/main/java/dev/project/scholar_ai/ScholarAiApplication.java
@@ -2,11 +2,13 @@ package dev.project.scholar_ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Main class for the application.
  */
 @SpringBootApplication
+@CrossOrigin
 public class ScholarAiApplication {
 
     public static void main(String[] args) {
