c3c6d52b0db83e224575b25f5b612159134323bc
Munim <munimthahmid2@gmail.com>
2025-05-25 18:57:38 +0600

    feat: Implemented web search agent

---
+++ src/main/java/dev/project/scholar_ai/dto/agentRequests/WebSearchRequest.java
@@ -0,0 +1,6 @@
+package dev.project.scholar_ai.dto.agentRequests;
+
+import java.util.List;
+import java.util.UUID;
+
+public record WebSearchRequest(UUID projectId, List<String> queryTerms, String domain, int batchSize, String correlationId) {} 
---
+++ src/main/java/dev/project/scholar_ai/messaging/publisher/WebSearchRequestSender.java
@@ -0,0 +1,21 @@
+package dev.project.scholar_ai.messaging.publisher;
+
+import dev.project.scholar_ai.config.RabbitMQConfig;
+import dev.project.scholar_ai.dto.agentRequests.WebSearchRequest;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebSearchRequestSender {
+    private final RabbitTemplate rt;
+    private final RabbitMQConfig rabbitMQConfig;
+
+    public WebSearchRequestSender(RabbitTemplate rabbitTemplate, RabbitMQConfig rabbitMQConfig) {
+        this.rt = rabbitTemplate;
+        this.rabbitMQConfig = rabbitMQConfig;
+    }
+
+    public void send(WebSearchRequest req) {
+        rt.convertAndSend(rabbitMQConfig.getExchangeName(), rabbitMQConfig.getWebSearchRoutingKey(), req);
+    }
+} 
