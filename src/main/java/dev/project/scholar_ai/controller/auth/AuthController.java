49ccfba3baf605e991f4efbbc4ec26a0ae5d0fa4
mstMetaly <hellomet79@gmail.com>
2025-06-02 21:40:52 +0600

    cookie managed at controller, Webconfig fixed, auth services tested

--- src/main/java/dev/project/scholar_ai/controller/auth/AuthController.java
+++ src/main/java/dev/project/scholar_ai/controller/auth/AuthController.java
@@ -1,5 +1,6 @@
 package dev.project.scholar_ai.controller.auth;
 
+import dev.project.scholar_ai.config.RedisConfig;
 import dev.project.scholar_ai.dto.auth.AuthDTO;
 import dev.project.scholar_ai.dto.auth.AuthResponse;
 import dev.project.scholar_ai.dto.auth.RefreshTokenRequest;
@@ -13,6 +14,8 @@ import jakarta.validation.Valid;
 import java.security.Principal;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -27,12 +30,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
     private final AuthService authService;
+
 
     @PostMapping("/register")
     public ResponseEntity<APIResponse<String>> register(
             @Valid @RequestBody AuthDTO authDTO, HttpServletRequest request) {
         try {
+            logger.info("/register endpoint hitted with request, " , request);
+
             authService.registerUser(authDTO.getEmail(), authDTO.getPassword());
             return ResponseEntity.status(HttpStatus.CREATED)
                     .body(APIResponse.success(HttpStatus.CREATED.value(), "User registered successfully", null));
@@ -49,12 +56,15 @@ public class AuthController {
             HttpServletResponse response) {
         try
         {
+            logger.info("login endpoint hitted with request,", request);
+
             AuthResponse authResponse = authService.loginUser(authDTO.getEmail(), authDTO.getPassword());
 
             //Create secure HttpOnly cookie for refresh token
             Cookie refreshCookie = new Cookie("refreshToken", authResponse.getRefreshToken());
             refreshCookie.setHttpOnly(true);//only over https
-            refreshCookie.setPath("/api/v1/auth/refresh");
+            refreshCookie.setSecure(false); // for local testing
+            refreshCookie.setPath("/");
             refreshCookie.setMaxAge(7 * 24 * 60 * 60);
 
             response.addCookie(refreshCookie);
@@ -62,6 +72,7 @@ public class AuthController {
             //remove refresh token from body before sending
             authResponse.setRefreshToken(null);
 
+            logger.info("response cookie added , authResponse: ", authResponse);
             return ResponseEntity.ok(APIResponse.success(HttpStatus.OK.value(), "Login successful", authResponse));
 
         }
@@ -79,6 +90,8 @@ public class AuthController {
     public ResponseEntity<APIResponse<AuthResponse>> refreshToken(
             @Valid @RequestBody RefreshTokenRequest refreshRequest, HttpServletRequest request) {
         try {
+            logger.info("refresh endpoint hitted");
+
             //Extract refresh token from cookies
             String refreshToken = null;
             if(request.getCookies() != null){
@@ -114,14 +127,16 @@ public class AuthController {
             HttpServletResponse response,
             Principal principal) {
         try{
+            logger.info("logout endpoint hitted");
+
             String email = principal.getName();
             authService.logoutUser(email);
 
             //Clear the cookie
             Cookie cookie = new Cookie("refreshToken", null);
             cookie.setHttpOnly(true);
-            cookie.setSecure(true);
-            cookie.setPath("/api/v1/auth/refresh");
+            cookie.setSecure(false);
+            cookie.setPath("/");
             cookie.setMaxAge(0);
 
             response.addCookie(cookie);
